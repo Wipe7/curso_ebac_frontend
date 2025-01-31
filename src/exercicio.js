98c6bc3c51e161c01382b4afc4d909112346858c
Wipe7 <wpeixototeixeira@gmail.com>
2025-01-31 19:12:34 -0300

    Adicionar exercicio TS

---
+++ src/exercicio.js
@@ -0,0 +1,12 @@
+"use strict";
+// Função de multiplicação
+function multiplicacao(a, b) {
+    return a * b;
+}
+// Função de saudação
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+// Exemplos de uso
+console.log(multiplicacao(2, 3)); // Retorna: 6
+console.log(saudacao("Maria")); // Retorna: "Olá Maria"
---
+++ src/exercicio.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicacao(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+// Exemplos de uso
+console.log(multiplicacao(2, 3));  // Retorna: 6
+console.log(saudacao("Maria"));    // Retorna: "Olá Maria"
